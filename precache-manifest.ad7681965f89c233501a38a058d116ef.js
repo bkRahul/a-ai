5f8f2c742bb0a2983c11af3f550864987e22ba12
bkRahul <rahulsatya1068@gmail.com>
2020-09-27 17:43:23 +0530

    Updates

---
+++ precache-manifest.ad7681965f89c233501a38a058d116ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258e94d5d3ff77affb423e8723c81ddd",
+    "url": "/a-ai/index.html"
+  },
+  {
+    "revision": "d5d52dea7845abb841d9",
+    "url": "/a-ai/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "eabbf2af454f2c4a48f5",
+    "url": "/a-ai/static/css/main.25c5f8dc.chunk.css"
+  },
+  {
+    "revision": "d5d52dea7845abb841d9",
+    "url": "/a-ai/static/js/2.30b7adcf.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/a-ai/static/js/2.30b7adcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eabbf2af454f2c4a48f5",
+    "url": "/a-ai/static/js/main.532324c7.chunk.js"
+  },
+  {
+    "revision": "6a02d0706f8674691817",
+    "url": "/a-ai/static/js/runtime-main.916449fb.js"
+  }
+]);
